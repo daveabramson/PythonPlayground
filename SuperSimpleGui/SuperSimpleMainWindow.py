6c6f22d0730ee076e89c987295945c4c4d8ecced
dabramson <dabramson@psi-software.com>
2019-08-29 16:21:44 -0400

    the gui now does something beside just sit and look pretty, though what it does isn't much more useful than that

--- SuperSimpleGui/SuperSimpleMainWindow.py
+++ SuperSimpleGui/SuperSimpleMainWindow.py
@@ -1,6 +1,6 @@
 from PySide2 import QtWidgets
 import SuperSimpleMainWindowUi
-
+import os
 
 
 class SuperSimpleMainWindow(QtWidgets.QMainWindow, SuperSimpleMainWindowUi.Ui_MainWindow):
@@ -9,5 +9,18 @@ class SuperSimpleMainWindow(QtWidgets.QMainWindow, SuperSimpleMainWindowUi.Ui_Ma
     def __init__(self, parent=None):
         super(SuperSimpleMainWindow, self).__init__(parent)
         self.setupUi(self)
+        self.btnBrowse.clicked.connect(self.BrowseFolder)
+        self.listWidget.itemClicked.connect(self.DisplayItem)
 
+    def BrowseFolder(self):
+        self.listWidget.clear()
+        directory = QtWidgets.QFileDialog.getExistingDirectory(self, "Pick a folder")
+        if directory:
+            self.listWidget.addItems(os.listdir(directory))
+
+    def DisplayItem(self):
+        msgBox = QtWidgets.QMessageBox(None)
+        msgBox.setWindowTitle("You picked this item")
+        msgBox.setText(self.listWidget.currentItem().text())
+        msgBox.exec_()
 
